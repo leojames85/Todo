efef49cbe7b6880a32f09a00409feec8c792f473
leojames85 <jlb.198550@gmail.com>
2023-06-22 18:17:41 -0400

    Solução do Leonardo

--- app.js
+++ app.js
@@ -1,4 +1,72 @@
+/* ----------MINHA RESOLUÇÃO----------- */
+
 'use strict'
+
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+const formSearchInput = document.querySelector('.form-search')
+
+const addTodo = inputValue => {
+    if (inputValue.length) {
+        todosContainer.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt " data-trash="${inputValue}"></i></li>`
+    }
+}
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+    const inputValue = event.target.add.value.trim()
+
+    addTodo(inputValue)
+
+    event.target.reset()
+})
+
+todosContainer.addEventListener('click', event => {
+    const dataTrashValue = event.target.dataset.trash
+    const todo = document.querySelector(`[data-todo="${dataTrashValue}"]`)
+
+    if (dataTrashValue) {
+        todo.remove()
+    }
+})
+
+const filterTodos = (todos, inputValue, returnMatchedTodos) => todos
+    .filter(todo => {
+        const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
+
+        return returnMatchedTodos ? matchedTodos : !matchedTodos
+    })
+
+const manipulateClasses = (todos, classToAdd, classToRemove) => {
+    todos.forEach( todo => {
+        todo.classList.remove(classToRemove)
+        todo.classList.add(classToAdd)
+    });
+}
+
+const hideTodos = (todos, inputValue) => {
+    const todosToHide = filterTodos(todos, inputValue, false)
+    manipulateClasses(todosToHide, 'hidden', 'd-flex')
+}
+
+const showTodos = (todos, inputValue) => {
+    const todosToShow = filterTodos(todos, inputValue, true)
+    manipulateClasses(todosToShow, 'd-flex', 'hidden')
+}
+
+formSearchInput.addEventListener('input', event => {
+    const inputValue = event.target.value.toLowerCase().trim()
+    const todos = Array.from(todosContainer.children)
+
+    hideTodos(todos, inputValue)
+    showTodos(todos, inputValue)
+})
+
+
+
+/*  ---------------RESOLUÇÃO DO PROFESSOR----------------
 
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
@@ -38,7 +106,7 @@ todosContainer.addEventListener('click', event => {
 
 const filterTodos = (todos, inputValue, returnMatchedTodos) => todos
     .filter(todo => {
-        const matchedTodos = todo.textContent.toLocaleLowerCase().includes(inputValue)
+        const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
 
         return returnMatchedTodos ? matchedTodos : !matchedTodos
     })
@@ -61,10 +129,10 @@ const showTodos = (todos, inputValue) => {
 }
 
 inputSearchTodo.addEventListener('input', event => {
-    const inputValue = event.target.value.trim().toLocaleLowerCase()
+    const inputValue = event.target.value.trim().toLowerCase()
     const todos = Array.from(todosContainer.children)
-
 
     hideTodos(todos, inputValue)
     showTodos(todos, inputValue)
 })
+ */
